ed344bc642ce182f8f77581bc0c9f2e2925ceb39
Sonia Diwedi <soniadiwedi1998@gmail.com>
2022-12-03 22:49:53 +0530

    done

---
+++ unit-3/sprint-3/day-4/assignments/oddandeven/src/oddEven.js
@@ -0,0 +1,18 @@
+function getData(data) {
+    return new Promise((resolve,reject)=>{
+        if(data===NaN){
+            reject ("error")
+        }else if(data%2==1){
+            setTimeout(()=>{
+                resolve ("odd")
+            },3000);
+        }else if(data%2==0){
+            setTimeout(()=>{
+                resolve ("even");
+            },4000)
+            
+        }
+    });
+}
+
+export default getData
