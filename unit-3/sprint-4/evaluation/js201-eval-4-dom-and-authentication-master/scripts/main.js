c67888c91da6b2270d9ae34cab09913823cf1ce3
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2022-12-12 17:22:47 +0530

    5

--- unit-3/sprint-4/evaluation/js201-eval-4-dom-and-authentication-master/scripts/main.js
+++ unit-3/sprint-4/evaluation/js201-eval-4-dom-and-authentication-master/scripts/main.js
@@ -10,7 +10,7 @@ const userRegisterURL = `${baseServerURL}/user/register`;
 const userLoginURL = `${baseServerURL}/user/login`;
 let paginationWrapper = document.getElementById("pagination-wrapper");
 
-let userName= document.getElementById("login-user-username");
+let loginUsername= document.getElementById("login-user-username");
 let loginUserPassword = document.getElementById("login-user-passowrd");
 
 let loginUserButton = document.getElementById("login-user");
@@ -25,67 +25,133 @@ const urlAllTodosOfUser = `${baseServerURL}/todos?userId=${userId}`;
 const urlTodosBase = `${baseServerURL}/todos/`;
 
 
-loginUserButton.addEventListener("click",(e)=>{
-  let inputs=document.querySelectorAll(".row input");
-  let object={
-    [inputs[0].placeholder]: inputs[0].value,
-    [inputs[1].placeholder]: inputs[1].value
+// loginUserButton.addEventListener("click",(e)=>{
+//   let inputs=document.querySelectorAll(".row input");
+//   let object={
+//     [inputs[0].placeholder]: inputs[0].value,
+//     [inputs[1].placeholder]: inputs[1].value
 
-  }
-  loginPage(object);
+//   }
+//   loginPage(object);
 
+// });
+//    async function loginPage(object){
+//   try{
+//     let res=await fetch(userLoginURL,{
+//       method:"POST",
+//       body:JSON.stringify(object),
+//       headers:{
+//         "Content-Type":"application/json"
+//       }
+//     });
+//       if(res.ok===true){
+//         alert("Logged In")
+//         let token=await res.json()
+//         //console.log(token)
+//         let userAuthToken=token.accessToken;
+//         let userId=token.user.id;
+//         localStorage.setItem("localAccessToken",userAuthToken);
+//         localStorage.setItem("userId",userIdclear);
+//         notificationWrapper.innerText=null;
+//         let head=document.createElement("h5")
+//         head.className="notification info"
+//         head.innerText=`hey ${obj.username}, welcome back!`
+//         notificationWrapper.append(head);
+//       }
+
+//   }catch(error){
+//     alert("err");
+//   }
+// }
+// //--------------------------------------------------------------------------------
+// async function fetchTodo(){
+// //   let res=await fetch(`${baseServerURL}/todos?userId=${userId}`,{
+// //     method:'GET',
+// //     headers:{
+// //       "Content-Type":"application/json",
+// //       Authorization : `Bearer ${localStorage.getItem("userAuthToken")}`
+// //     }
+// //   })
+// //   if(res.ok){
+// //     let out=await res.json();
+// //     console.log(out);
+// //   }else{
+// //     alert("Done")
+// //   }
+// // }
+
+
+// // let render=(out)=>{
+
+// try{
+//   let fetch_todo=await fetch(`${baseServerURL}/todos?userId=${userId}`,{
+//     method:'GET',
+//     headers:{
+//       "Content-Type":"application/json",
+//       Authorization : `Bearer ${localStorage.getItem("userAuthToken")}`
+//     }
+//   })
+// }
+
+// }
+
+// loginUserButton.addEventListener("click",async function (){
+//   let object={username:loginUsername.value,password:loginUserPassword.value};
+//   let response=await fetch(userLoginURL,{
+//     method:{
+//       "Content-Type":"application/json"
+//     },
+//     body:JSON.stringify(object)
+//   })
+//   if(response.ok){
+//     let out=await response.json()
+//     console.log(out)
+//     localStorage.setItem("localAccessToken",out.accessToken);
+//         localStorage.setItem("userId",+out.user.id);
+//         notificationWrapper.innerHTML=`<h5 class="notification info">
+//         hey ${object.username}, welcome back!
+//     </h5>`
+//   }else{
+//     console.log("Failed")
+//   }
+// })
+//--------------------------------------------------------------------------
+loginUserButton.addEventListener("click", (e) => {
+  let objects = {};
+  obj["username"] = loginUserUsername.value;
+  obj["password"] = loginUserPassword.value;
+
+  getData(objects);
 });
-   async function loginPage(object){
-  try{
-    let res=await fetch(userLoginURL,{
-      method:"POST",
-      body:JSON.stringify(object),
-      headers:{
-        "Content-Type":"application/json"
-      }
+
+async function getData(objects) {
+  try {
+    let res = await fetch(userLoginURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(objects),
     });
-      if(res.ok===true){
-        alert("Logged In")
-        let token=await res.json()
-        //console.log(token)
-        let userAuthToken=token.accessToken;
-        let userId=token.user.id;
-        localStorage.setItem("localAccessToken",userAuthToken);
-        localStorage.setItem("userId",userIdclear);
-        notificationWrapper.innerText=null;
-        let head=document.createElement("h5")
-        head.className="notification info"
-        head.innerText=`hey ${obj.userName}, welcome back!`
-        notificationWrapper.append(head);
+    if (res.ok) {
+      let data = await fetching.json();
 
-        
-        
-    
+      localStorage.setItem("userId", +data.user.id);
+      localStorage.setItem("localAccessToken", data.accessToken);
 
-      }
-
-  }catch(error){
-    alert("err");
-  }
-}
-
-let fetchTodo=async () =>{
-  let res=await fetch(`${baseServerURL}/todos?userId=${userId}`,{
-    method:'GET',
-    headers:{
-      "Content-Type":"application/json",
-      Authorization : `Bearer ${localStorage.getItem("userAuthToken")}`
+      notificationWrapper.innerHTML = `
+      <h5 class="notification info">
+          hey ${objects.username}, welcome back!
+      </h5>
+`;
+    } else {
+      console.log("failed");
     }
-  })
-  if(res.ok){
-    let out=await res.json();
-    console.log(out);
-  }else{
-    alert("Done")
+  } catch (error) {
+    alert("error");
   }
 }
 
-
-let render=(out)=>{
-
-}
+getTodoButton.addEventListener("click", (e) => {
+  FetchTodos();
+});
