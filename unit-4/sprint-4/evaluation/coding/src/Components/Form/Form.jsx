4bc1d6cac807688f7871b0a57dedd7b1b01eb49d
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-01-16 16:46:19 +0530

    done

--- unit-4/sprint-4/evaluation/coding/src/Components/Form/Form.jsx
+++ unit-4/sprint-4/evaluation/coding/src/Components/Form/Form.jsx
@@ -1,33 +1,99 @@
-
+import {
+  FormControl,
+  Input,
+  Checkbox,
+  FormLabel,
+  Button,
+} from "@chakra-ui/react";
+import { useRef } from "react";
+import axios from "axios";
 export default function Form() {
-  
-// TODO: Remove below const and instead import them from chakra
-  const Button = () => <div />;
-  const Checkbox = () => <div />;
-  const FormControl = () => <div />;
-  const FormLabel = () => <div />;
-  const Input = () => <div />;
+  // TODO: Remove below const and instead import them from chakra
+  let userRef = useRef({
+    name: "",
+    ownerName: "",
+    address: "",
+    areaCode: "",
+    rent: "",
+    isBachelorAllowed: false,
+    isMarriedAllowed: false,
+  });
+  const inputData = (e) => {
+    if (e.target.type == "checkbox") {
+      userRef.current[e.target.name] = e.target.checked;
+    } else {
+      userRef.current[e.target.name] = e.target.value;
+    }
+    console.log(userRef.current);
+  };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        `http://localhost:${process.env.REACT_APP_JSON_SERVER_PORT}/houses`,
+        userRef.current
+      )
+      .then((res) => {
+        console.log(res);
+      });
+  };
   return (
-      <div className = "addHouseContainer" >
-        <form className = "form" >
-          <FormControl>
-              <Input className = "name" placeholder = "Name" />
-              <Input className = "ownerName" placeholder = "Owners name" />
-              <Input className = "address" placeholder = "Address" />
-              <Input className = "areaCode" placeholder = "Area code" />
-              <Input className = "rent" placeholder = "Rent" />
-              <Checkbox className = "bachelor" >
-                <FormLabel>Married Tenants Allowed</FormLabel>
-              </Checkbox>
-              <br />
-              <Checkbox className = "married" >
-                <FormLabel>Bachelor Tenants Allowed</FormLabel>
-              </Checkbox>
-              <br />
-              <Button className = "submitBtn" > Submit</Button>
-          </FormControl>
-        </form>
-      </div>
-  )
+    <div className="addHouseContainer">
+      <form className="form" onSubmit={onSubmit}>
+        <FormControl>
+          <Input
+            className="name"
+            placeholder="Name"
+            name="name"
+            onChange={inputData}
+          />
+          <Input
+            className="ownerName"
+            placeholder="Owners name"
+            name="ownerName"
+            onChange={inputData}
+          />
+          <Input
+            className="address"
+            placeholder="Address"
+            name="address"
+            onChange={inputData}
+          />
+          <Input
+            className="areaCode"
+            placeholder="Area code"
+            name="areaCode"
+            onChange={inputData}
+          />
+          <Input
+            className="rent"
+            placeholder="Rent"
+            name="rent"
+            onChange={inputData}
+          />
+          <Checkbox
+            className="bachelor"
+            onChange={inputData}
+            name="isBachelorAllowed"
+          >
+            <FormLabel>Married Tenants Allowed</FormLabel>
+          </Checkbox>
+          <br />
+          <Checkbox
+            className="married"
+            onChange={inputData}
+            name="isMarriedAllowed"
+          >
+            <FormLabel>Bachelor Tenants Allowed</FormLabel>
+          </Checkbox>
+          <br />
+          <Button className="submitBtn" type="submit">
+            {" "}
+            Submit
+          </Button>
+        </FormControl>
+      </form>
+    </div>
+  );
 }
