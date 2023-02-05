5717274d9db898b7b0337fb7632a3c18055d3655
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-02-05 17:24:35 +0530

    1

--- unit-5/sprint-2/day-4/we/masai-ops-825-1351-sIOMqY-07ef695893551ddc9e8b0dc45617a629ed999e8e/src/Components/Counter.jsx
+++ unit-5/sprint-2/day-4/we/masai-ops-825-1351-sIOMqY-07ef695893551ddc9e8b0dc45617a629ed999e8e/src/Components/Counter.jsx
@@ -2,23 +2,16 @@ import React from "react";
 import { useSelector } from "react-redux";
 import CounterButtons from "./CounterButtons";
 import CounterValue from "./CounterValue";
-import  counter from "./counter.css"
+import  "./counter.css"
 const Counter = () => {
   const theme=useSelector((store)=>{
     return store.themeReducer.theme
   })
 
-  const style1={
-    color: "white",
-    backgroundColor: "black",
-  }  
-  const style2={
-    color: "black",
-    backgroundColor: "white",
-  }  
+  
   return (
     // the following div classname should be chnaged accrding to the theme
-    <div data-testid={theme==="light"?"dark_theme":"light_theme"} >
+    <div data-testid="counter"   className={theme==="dark"?"dark_theme":"light_theme"}  >
       <h1>Counter</h1>
       {/* Import CounterValue component here and DO NOT PASS anything through props, for this component */}
       <CounterValue/>
