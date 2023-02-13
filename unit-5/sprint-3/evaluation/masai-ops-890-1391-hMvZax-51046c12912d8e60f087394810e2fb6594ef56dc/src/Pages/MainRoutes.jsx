5f6fb2c82da87ba643653704d07de9e4c1fac3b1
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-02-13 17:32:00 +0530

    1

--- unit-5/sprint-3/evaluation/masai-ops-890-1391-hMvZax-51046c12912d8e60f087394810e2fb6594ef56dc/src/Pages/MainRoutes.jsx
+++ unit-5/sprint-3/evaluation/masai-ops-890-1391-hMvZax-51046c12912d8e60f087394810e2fb6594ef56dc/src/Pages/MainRoutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Private from "../Components/Private";
 import { Homepage } from "./Homepage";
 import { Login } from "./Login";
 import { MovieDetail } from "./MovieDetail";
@@ -8,7 +9,7 @@ export const MainRoutes = () => {
   return <Routes>{/* Provide all routes here */}
         <Route path="/" element={<Homepage/>}/>
         <Route path="/login" element={<Login/>}/>
-        <Route path="/movie/:id" element={<MovieDetail/>}/>
+        <Route path="/movie/:id" element={<Private><MovieDetail/></Private>}/>
 
   </Routes>;
 };
