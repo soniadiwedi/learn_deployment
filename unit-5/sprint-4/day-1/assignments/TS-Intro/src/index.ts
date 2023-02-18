229e74b907228da3a792dc387319d67d09e06f66
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-02-18 13:23:30 +0530

    type

---
+++ unit-5/sprint-4/day-1/assignments/TS-Intro/src/index.ts
@@ -0,0 +1,47 @@
+// //Number
+// let a:number=5;
+
+// //String
+// let s1:string="str1"
+// let s2:string="str2"
+
+// //Boolean
+// let bo=false
+// let bol:boolean=true
+
+// let result:string=`${a} ${s2}`
+// console.log(a);
+
+
+let Name:string="sonia"
+//----------------------------
+let age:number=24;
+//------------------------------
+let isFetching:boolean=true
+
+//-----------------------------
+let arr:number[]=[1,2,3]
+//----------------------------
+let arrString:string[]=["a","b","c"]
+//------------------------------
+
+let tuple:[string,boolean,]=["sonia",true]
+//------------------------------------------
+// create an enum
+ enum userdata{
+    User,
+SuperUser,
+Admin,
+SuperAdmin
+ }
+
+ // create a function that takes 2 arguments x,y, both numbers
+
+ function product(x:number,y:number){
+    return x*y
+ }
+
+ function divide(x:number,y:number){
+    return x/y
+ }
+
