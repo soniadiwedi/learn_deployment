8c11c5b4c14ea9b17684004549b08d2480f6dc4b
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-03-20 15:13:13 +0530

    patch and get

--- unit-6/sprint-3/evaluation/movie_evaluation/routes/movie.route.js
+++ unit-6/sprint-3/evaluation/movie_evaluation/routes/movie.route.js
@@ -13,4 +13,33 @@ router.post("/add",async(req,res)=>{
     }
 })
 
+
+// router.get("/",async(req,res)=>{
+//     try{
+//         const {min,max,year_of_release}
+//     }
+// })
+
+
+router.get("/:id",async(req,res)=>{
+    const {id}=req.params
+    try{
+        let movie=await MovieModel.findById(id)
+        res.status(200).send({movie})
+    }catch(err){
+        res.status(500).send("server error")
+    }
+})
+
+
+router.patch("/update/:id",async(req,res)=>{
+    const {id}=req.params
+    const data=req.body
+    try{
+        await MovieModel.findByIdAndUpdate({_id:id},data)
+        res.status(200).send({"msg":"movie has been updated"})
+    }catch(err){
+        res.status(400).send({"msg":err.message})
+    }
+})
 module.exports={router}
